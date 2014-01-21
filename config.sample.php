349f8733456e32da97b2079e98a4c3415fb0cb66
Mostafa Mirmousavi <mirmousavi.m@gmail.com>
2014-01-21 14:33:37 +0330

    Add database config in config file

--- config.sample.php
+++ config.sample.php
@@ -3,3 +3,7 @@
 define('CONSUMER_KEY', 'CONSUMER_KEY_HERE');
 define('CONSUMER_SECRET', 'CONSUMER_SECRET_HERE');
 define('OAUTH_CALLBACK', 'http://example.com/sms2twitt/callback.php');
+
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
